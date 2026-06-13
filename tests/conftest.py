1c1dbde10ef12b7f4fe184cd8e3931081077a52b
Artem <arte.kats@gmail.com>
2026-06-13 19:18:20 +0300

    Fix: GDPR cookie popup conftest.py - добавлена блокировка GDPR cookie popup через Chrome DevTools Protocol

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,8 +7,15 @@ def browser():
     '''Фикстура запуска браузера для UI тестов'''
     # Настройка драйвера
     options = webdriver.ChromeOptions()
-    # options.add_argument("--headless") 
+
+    # options.add_argument("--headless")
+
     browser = webdriver.Chrome(options=options)
+    browser.execute_cdp_cmd('Network.setBlockedURLs', {
+        'urls': ['*fundingchoicesmessages.google.com*', '*pagead2.googlesyndication.com*']
+    })
+    browser.execute_cdp_cmd('Network.enable', {})
+
     browser.maximize_window()
     yield browser
     browser.quit()
